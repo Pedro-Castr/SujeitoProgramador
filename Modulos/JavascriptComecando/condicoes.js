8b4d24f1dc1056eafe627661f88bf1810ec66187
Pedro Castro <vieiracastropedro7@gmail.com>
2026-03-24 20:43:04 -0300

    Estudos dia 24/03/2026 20:43

---
+++ Modulos/JavascriptComecando/condicoes.js
@@ -0,0 +1,35 @@
+function pedir() {
+  var valor = prompt("Faça seu pedido:")
+
+  switch (Number(valor)) {
+    case 1:
+      alert("Você escolheu suco!")
+      break
+    case 2:
+      alert("Você escolheu Ãgua Gelada!")
+      break
+    case 3:
+      alert("Você escolheu Sorvete!")
+      break
+    case 4:
+      alert("Você chamou o garçom!")
+      break
+    default:
+      alert("Escolha um valor válido!")
+  }
+}
+
+var nome = "Pedro";
+var userOnline = true;
+
+if (userOnline == true) {
+  console.log("Usuário online")
+} else if (nome == "Pedro") {
+  console.log("Bem vindo, Pedro")
+} else {
+  console.log("Usuário offline")
+}
+
+var num = 10;
+
+num === 10 ? console.log("o valor é 10") : console.log("o valor não é 10")
---
+++ Modulos/JavascriptComecando/loops.js
@@ -0,0 +1,10 @@
+var x = 0;
+
+while (x < 10) {
+  document.writeln('<br>O valor do X é: ' + x);
+  x++;
+}
+
+for (var z = 0; z < 10; z++) {
+  document.writeln('<br>O valor do Z é: ' + z);
+}
