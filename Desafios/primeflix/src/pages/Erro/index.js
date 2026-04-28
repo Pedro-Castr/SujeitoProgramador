7941441a81d4c6be05fdaecd0565c959a5196599
Pedro Castro <vieiracastropedro7@gmail.com>
2026-04-27 21:04:27 -0300

    cria erro padrão de página não encontrada e adiciona nas rotas

---
+++ Desafios/primeflix/src/pages/Erro/index.js
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+import './erro.css';
+
+function Erro() {
+  return(
+    <div className="not-found">
+      <h1>Ops!</h1>
+      <h3>A página que você buscou não foi encontrada.</h3>
+      <p>Retornar para a <Link to='/'>Home</Link></p>
+    </div>
+  );
+}
+
+export default Erro;
