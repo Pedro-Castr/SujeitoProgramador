4c7edeb8c2594c0125f69f9f0ebf13fc2a0dff99
Pedro Castro <vieiracastropedro7@gmail.com>
2026-04-27 21:06:21 -0300

    modifica e estiliza página de filmes

--- Desafios/primeflix/src/pages/Filme/index.js
+++ Desafios/primeflix/src/pages/Filme/index.js
@@ -1,7 +1,56 @@
+import { useEffect, useState } from "react";
+import { useParams } from 'react-router-dom';
+import api from "../../services/api";
+import './filme-info.css'
+
 function Filme() {
+  const { id } = useParams();
+  const [filme, setFilme] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadFilme() {
+      await api.get(`movie/${id}`, {
+        params: {
+          api_key: 'e41d651f9fc642229cd44ae081e4b305',
+          language: 'pt-BR',
+        }
+      })
+      .then((response) => {
+        setFilme(response.data);
+        setLoading(false);
+      })
+      .catch(() => {
+        
+      })
+    }
+
+    loadFilme();
+  }, [])
+
+  if (loading) {
+    return(
+      <div className="filme-info">
+        <h2>Carregando detalhes do filme...</h2>
+      </div>
+    );
+  }
+
   return(
-  <div>
-    <h1>Bem vindo a página de filmes</h1>
+  <div className="filme-info">
+    <h1>{filme.title}</h1>
+    <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
+
+    <h3>Sinopse</h3>
+    <span>{filme.overview}</span>
+    <strong>Avaliação: {filme.vote_average}/10</strong>
+
+    <div className="area-buttons">
+      <button>Salvar</button>
+      <button>
+        <a href="#">Trailer</a>
+      </button>
+    </div>
   </div>
   );
 }
