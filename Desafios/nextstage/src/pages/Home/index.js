dccc82edd42c4837c168d101c0ca1f561c251246
Pedro Castro <vieiracastropedro7@gmail.com>
2026-05-12 20:52:01 -0300

    feat: configura e utiliza API do `rawg` para jogos

--- Desafios/nextstage/src/pages/Home/index.js
+++ Desafios/nextstage/src/pages/Home/index.js
@@ -1,4 +1,29 @@
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+
 function Home() {
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    async function loadGames() {
+      try {
+        const response = await api.get("/games", {
+          params: {
+            key: "2b081d046b334f76b1cf7b4b01845c4b",
+            page_size: 10,
+            ordering: "-rating",
+            languages: "pt",
+          },
+        });
+        setGames(response.data.results);
+      } catch (error) {
+        console.log("ERRO:", error);
+      }
+    }
+    loadGames();
+  }, []);
+
+  console.log(games);
   return <div></div>;
 }
 
