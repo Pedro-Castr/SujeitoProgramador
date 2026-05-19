cd8dcc14f3dbf99752f8507dd0541c83bf59bd79
Pedro Castro <vieiracastropedro7@gmail.com>
2026-05-19 19:59:30 -0300

    feat: adiciona rota para página game e rota padrão de erro

--- Desafios/nextstage/src/routes.js
+++ Desafios/nextstage/src/routes.js
@@ -1,13 +1,30 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Outlet } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Game from "./pages/Game";
+import Error from "./pages/Error";
+
+function MainLayout() {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+}
 
 function RoutesApp() {
   return (
     <BrowserRouter>
-      <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/game/:id" element={<Game />} />
+        </Route>
+
+        <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
   );
