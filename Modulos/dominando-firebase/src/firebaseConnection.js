6ed9f2eac718085d8890c9b72e0076106908e9d9
Pedro Castro <vieiracastropedro7@gmail.com>
2026-06-02 20:12:32 -0300

    fix: corrige exposição dos dados de conexão com `firebase`

--- Modulos/dominando-firebase/src/firebaseConnection.js
+++ Modulos/dominando-firebase/src/firebaseConnection.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDS0EsIJi289fVypmhJRbSQISld7WK2jJU",
-  authDomain: "curso-7206c.firebaseapp.com",
-  projectId: "curso-7206c",
-  storageBucket: "curso-7206c.firebasestorage.app",
-  messagingSenderId: "162274792083",
-  appId: "1:162274792083:web:60f46e1f1bb743e6c77366",
-  measurementId: "G-NVHZTWQWTL",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
