00e79da46c6510a65f8a2566d5f832c4599afcd4
Pedro Castro <vieiracastropedro7@gmail.com>
2026-06-04 21:14:09 -0300

    feat: adiciona arquivo de configuração do typescript

--- Modulos/evoluindo-typescript/aula1.ts
+++ Modulos/evoluindo-typescript/aula1.ts
@@ -1,3 +1,13 @@
 const nome = "Pedro Castro";
 
 console.log(`Seja bem vindo, ${nome}!`);
+
+class Sistema {
+  ip: String;
+
+  constructor(ip: String) {
+    this.ip = ip;
+  }
+}
+
+const servidor1 = new Sistema("132.134.642.87");
