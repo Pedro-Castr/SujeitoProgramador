1cebcc03bb3cdd251a57a0aac076cf483365bcd0
Pedro Castro <vieiracastropedro7@gmail.com>
2026-06-04 21:26:40 -0300

    feat: ajusta `rootDir` e `outDir` no arquivo `tsconfig.json`

---
+++ Modulos/evoluindo-typescript/build/aula1.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const nome = "Pedro Castro";
+console.log(`Seja bem vindo, ${nome}!`);
+//# sourceMappingURL=aula1.js.map
