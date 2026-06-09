e7b1d6ff9ac38cc4bb71408e081c444fa3dec52a
Pedro Castro <vieiracastropedro7@gmail.com>
2026-06-09 20:17:20 -0300

    feat: adiciona mais tipos de variáveis do typescript

--- Modulos/evoluindo-typescript/src/types.ts
+++ Modulos/evoluindo-typescript/src/types.ts
@@ -22,3 +22,32 @@ preco = "Pedro";
 // type boolean = aceita valores de verdadeiro ou falso
 let autenticado: boolean = false;
 console.log(autenticado);
+
+// type array
+let somenteNumeros: number[];
+somenteNumeros = [4, 56, 87, 13];
+somenteNumeros.push(33);
+console.log(somenteNumeros);
+
+let somenteString: Array<string>;
+somenteString = ["Pedro", "Luiza", "Matheus"];
+somenteString.push("Sujeito Programador");
+console.log(somenteString);
+
+// let arrayVariado: (string | number)[];
+let arrayVariado: Array<string | number>;
+arrayVariado = [32, "Pedro", 12, "Luiza"];
+console.log(arrayVariado);
+
+// tuplas: especifica a quantidade de itens e qual o tipo de cada um. Não tem no JS, somente no TS
+let aluno: [string, number];
+aluno = ["Pedro Castro", 21];
+console.log(aluno);
+
+// type object
+let novoUsuario: object = {
+  nome: "Pedro Castro",
+  email: "pedro@gmail.com",
+  idade: 21,
+};
+console.log(novoUsuario);
