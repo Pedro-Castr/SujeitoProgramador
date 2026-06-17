7dca57bd0cfecf200c689424444897f1e5a0dc92
Pedro Castro <vieiracastropedro7@gmail.com>
2026-06-17 19:36:51 -0300

    feat: adiciona métodos de exemplo a classe

--- Modulos/typescript-classe-metodo-generic/src/classe.ts
+++ Modulos/typescript-classe-metodo-generic/src/classe.ts
@@ -7,7 +7,32 @@ class Loja {
   constructor(nome: string, categoria: string) {
     ((this.nome = nome), (this.categoria = categoria));
   }
+
+  // Métodos
+  criarLoja(): void {
+    console.log(`Loja ${this.nome} criada com sucesso!`);
+  }
+
+  emitirPedido(mesa: number, ...pedidos: string[]): string {
+    pedidos.map((pedido) => {
+      console.log(`Saindo novo pedido: ${pedido}`);
+    });
+
+    return `Pedido da mesa número ${mesa}`;
+  }
 }
 
 const blueBurger = new Loja("Blue Burger", "Hamburgueria");
 console.log(blueBurger);
+blueBurger.criarLoja();
+
+const redPizza = new Loja("Red Pizza", "Pizzaria");
+console.log(redPizza);
+console.log(
+  redPizza.emitirPedido(
+    12,
+    "Pizza de catupiri",
+    "Pizza de calabresa",
+    "Pizza de frango",
+  ),
+);
