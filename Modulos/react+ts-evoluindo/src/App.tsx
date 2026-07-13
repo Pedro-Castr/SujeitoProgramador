b0f7b0b2ebf869fa920c2ddc287dc5b2c861f617
Pedro Castro <vieiracastropedro7@gmail.com>
2026-07-13 18:07:47 -0300

    feat: pratica mais sobre `useState`

--- Modulos/react+ts-evoluindo/src/App.tsx
+++ Modulos/react+ts-evoluindo/src/App.tsx
@@ -4,9 +4,18 @@ import { Header } from "./components/Header/header";
 import { Aluno } from "./components/Aluno/aluno";
 import { Footer } from "./components/Footer/footer";
 
+interface UserProps {
+  nome: string;
+  cargo: string;
+}
+
 export default function App() {
   const [cor, setCor] = useState("");
   const [contador, setContador] = useState(0);
+  const [user, setUser] = useState<UserProps>({
+    nome: "Visitante",
+    cargo: "",
+  });
 
   function adicionar() {
     setContador((valorAtual) => valorAtual + 1);
@@ -17,6 +26,20 @@ export default function App() {
       return;
     }
     setContador((valorAtual) => valorAtual - 1);
+  }
+
+  function handleLogin() {
+    setUser({
+      nome: "Pedro Castro",
+      cargo: "Dev",
+    });
+  }
+
+  function handleLogout() {
+    setUser({
+      nome: "Visitante",
+      cargo: "",
+    });
   }
 
   return (
@@ -37,6 +60,14 @@ export default function App() {
         <h2>Contador com useState</h2>
         <button onClick={adicionar}>+</button> {contador}{" "}
         <button onClick={diminuir}>-</button>
+      </div>
+
+      <div>
+        <h2>Utilizando useState</h2>
+        <button onClick={handleLogin}>Entrar</button>
+        <button onClick={handleLogout}>Sair</button>
+        <p>Olá, {user.nome}</p>
+        <strong>{user.cargo}</strong>
       </div>
 
       <Footer nome="Sujeito Programador" ano={2026} />
