fff2c824293f3b811fc30620a73d5dc5b2e52570
Pedro Castro <vieiracastropedro7@gmail.com>
2026-07-13 19:25:05 -0300

    feat: remove arquivos anteriores e reformula página principal para os exercícios do módulo

--- Modulos/react+ts-evoluindo/src/App.tsx
+++ Modulos/react+ts-evoluindo/src/App.tsx
@@ -1,76 +1,29 @@
-import { useState } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import CalculadoraCombustivel from "./pages/calculadoraCombustivel/calculadoraCombustivel";
+import "./App.css";
 
-import { Header } from "./components/Header/header";
-import { Aluno } from "./components/Aluno/aluno";
-import { Footer } from "./components/Footer/footer";
+function Home() {
+  return (
+    <div>
+      <h1 className="titulo">Atividades desenvolvidas nesse módulo</h1>
 
-interface UserProps {
-  nome: string;
-  cargo: string;
+      <ul className="lista">
+        <li>
+          <Link to="/calculadoraCombustivel">Calculadora de Combustível</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default function App() {
-  const [cor, setCor] = useState("");
-  const [contador, setContador] = useState(0);
-  const [user, setUser] = useState<UserProps>({
-    nome: "Visitante",
-    cargo: "",
-  });
-
-  function adicionar() {
-    setContador((valorAtual) => valorAtual + 1);
-  }
-
-  function diminuir() {
-    if (contador === 0) {
-      return;
-    }
-    setContador((valorAtual) => valorAtual - 1);
-  }
-
-  function handleLogin() {
-    setUser({
-      nome: "Pedro Castro",
-      cargo: "Dev",
-    });
-  }
-
-  function handleLogout() {
-    setUser({
-      nome: "Visitante",
-      cargo: "",
-    });
-  }
-
   return (
-    <div style={{ backgroundColor: cor }}>
-      <Header titulo="Curso React + TypeScript" />
-      <Aluno nome="Pedro Castro" idade={22} endereco="Rua dos Fodas" />
-
-      <div>
-        <h2>Altere a cor do site!</h2>
-        <input
-          placeholder="Digite a cor aqui"
-          value={cor}
-          onChange={(e) => setCor(e.target.value)}
-        />
-      </div>
-
-      <div>
-        <h2>Contador com useState</h2>
-        <button onClick={adicionar}>+</button> {contador}{" "}
-        <button onClick={diminuir}>-</button>
-      </div>
-
-      <div>
-        <h2>Utilizando useState</h2>
-        <button onClick={handleLogin}>Entrar</button>
-        <button onClick={handleLogout}>Sair</button>
-        <p>Olá, {user.nome}</p>
-        <strong>{user.cargo}</strong>
-      </div>
-
-      <Footer nome="Sujeito Programador" ano={2026} />
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route
+        path="/calculadoraCombustivel"
+        element={<CalculadoraCombustivel />}
+      />
+    </Routes>
   );
 }
